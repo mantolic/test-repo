82574b2a9193c8fe94e58c7a04a26cc45364df74
mantolic <mantolic@xebialabs.com>
2018-06-12 14:02:17 +0200

    Test version 3.

---
+++ Template 1.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Mateas-MBP:5516/#/templates/Folder72109daa311a41d9afa7b493be9a3a42-Folder6732961c384f400a801b435f3a14c8b7-Release83aec7e62c9640e192160cd381bf4be3/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Tue Jun 12 14:02:16 CEST 2018
+
+xlr {
+  template('Template 1') {
+    folder('Folder 1/Folder 2')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-06-12T09:00:00+0200')
+    phases {
+      phase('Phase 1') {
+        color '#0099CC'
+      }
+    }
+    
+  }
+}
