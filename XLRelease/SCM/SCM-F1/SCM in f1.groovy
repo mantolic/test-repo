f9578d9ec162b280079ffb5d45c68fe5e4f23b4d
mantolic <mantolic@xebialabs.com>
2018-08-02 15:14:08 +0200

    Test second

---
+++ XLRelease/SCM/SCM-F1/SCM in f1.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Mateas-MacBook-Pro.local:5516/#/templates/Folder9224c94606e149319d98c0f199224784-Folder2c24ff675ce5467690d8586205379948-Releaseb7463bee883e4f6bb59170976da5bfd5/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Thu Aug 02 15:14:07 CEST 2018
+
+xlr {
+  template('SCM in f1') {
+    folder('SCM/SCM-F1')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
