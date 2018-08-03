ba08fac470fc1c2e249f67fbd46d091211a61fc1
mantolic <mantolic@xebialabs.com>
2018-08-03 16:18:24 +0200

    Third

--- XLRelease/SCM/SCM-F1/SCM in f1.groovy
+++ XLRelease/SCM/SCM-F1/SCM in f1.groovy
@@ -1,6 +1,6 @@
-// Exported from:        http://Mateas-MacBook-Pro.local:5516/#/templates/Folder9224c94606e149319d98c0f199224784-Folder2c24ff675ce5467690d8586205379948-Releaseb7463bee883e4f6bb59170976da5bfd5/releasefile
+// Exported from:        http://Mateas-MBP:5516/#/templates/Folder9224c94606e149319d98c0f199224784-Folder2c24ff675ce5467690d8586205379948-Releaseb7463bee883e4f6bb59170976da5bfd5/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 02 15:14:07 CEST 2018
+// Date created:         Fri Aug 03 16:18:23 CEST 2018
 
 xlr {
   template('SCM in f1') {
@@ -8,7 +8,12 @@ xlr {
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
     phases {
       phase('New Phase') {
-        
+        color '#0099CC'
+        tasks {
+          createRelease('t1') {
+            
+          }
+        }
       }
     }
     
