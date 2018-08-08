ff2d2036d8362892bdc4e5b7dc120c63ac1d0bb1
mantolic <mantolic@xebialabs.com>
2018-08-08 13:24:36 +0200

    Fourth

--- XLRelease/SCM/SCM-F1/SCM in f1.groovy
+++ XLRelease/SCM/SCM-F1/SCM in f1.groovy
@@ -1,13 +1,13 @@
 // Exported from:        http://Mateas-MBP:5516/#/templates/Folder9224c94606e149319d98c0f199224784-Folder2c24ff675ce5467690d8586205379948-Releaseb7463bee883e4f6bb59170976da5bfd5/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Fri Aug 03 16:18:23 CEST 2018
+// Date created:         Wed Aug 08 13:24:35 CEST 2018
 
 xlr {
   template('SCM in f1') {
     folder('SCM/SCM-F1')
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
     phases {
-      phase('New Phase') {
+      phase('Phase') {
         color '#0099CC'
         tasks {
           createRelease('t1') {
