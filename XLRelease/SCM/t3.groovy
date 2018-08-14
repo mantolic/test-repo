69df44a92a8012754c0bcf06c16970cf04f437da
mantolic <mantolic@xebialabs.com>
2018-08-14 15:20:36 +0200

    f

---
+++ XLRelease/SCM/t3.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Mateas-MBP:5516/#/templates/Folder27895f8aca54d99ac57ecc3d7e5da9c-Release75cf85a822454b999368b71e2c466934/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Tue Aug 14 15:20:35 CEST 2018
+
+xlr {
+  template('t3') {
+    folder('SCM')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-14T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
