2f0c3af0a4b70f35349ad54768e1c2d1c6df1985
Release Administrator <>
2021-07-05 16:02:23 +0200

    second version

--- XLRelease/test trigger/template with trigger.groovy
+++ XLRelease/test trigger/template with trigger.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://MacBook-Pro.local:5517/101/#/templates/Folder09aeb33894cb44e792aabb53bc851df2-Release65b44ede21dc461aa19060cf65e9de87/code
-// Release version:      10.1.2
-// Date created:         Mon Jul 05 15:32:24 CEST 2021
+// Release version:      10.2.0-SNAPSHOT
+// Date created:         Mon Jul 05 16:02:23 CEST 2021
 
 xlr {
   template('template with trigger') {
@@ -28,6 +28,20 @@ xlr {
               attachment("sql:Attachment7b9767294d3548a08037d86a3240358e") {
                 filename "attachments/Attachment7b9767294d3548a08037d86a3240358e-🌈 Icon color.svg"
               }
+            }
+          }
+          manual('script') {
+            
+          }
+          custom('task') {
+            attachments {
+              attachment("sql:Attachmenta965bfb7f5d140a5a2cb1e3445a908e1") {
+                filename "attachments/Attachmenta965bfb7f5d140a5a2cb1e3445a908e1-tmpl_temp.yaml"
+              }
+            }
+            script {
+              type 'remoteScript.Unix'
+              
             }
           }
         }
