b2dddcda92a3139198bb8a96cd6f947b89957027
Release Administrator <>
2021-07-05 11:40:18 +0200

    first version

---
+++ XLRelease/trigger/template with trigger.groovy
@@ -0,0 +1,24 @@
+// Exported from:        http://MacBook-Pro.local:5516/#/templates/Folder8ca5deb2f15543f3ba31198dc5662323-Release03056070a2aa49ddaa52e3a133e5a2ca/code
+// Release version:      10.2.0-702.113
+// Date created:         Mon Jul 05 11:40:18 CEST 2021
+
+xlr {
+  template('template with trigger') {
+    folder('trigger')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-07-05T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('bla') {
+            attachments {
+              attachment("sql:Attachment4743063124004d1fa00c19373054eb18") {
+                filename "attachments/Attachment4743063124004d1fa00c19373054eb18-🌈 Icon color.svg"
+              }
+            }
+          }
+        }
+      }
+    }
+    
+  }
+}
