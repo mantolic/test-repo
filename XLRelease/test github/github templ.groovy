11f900ba163ed7f5c88a58742e4a29ac86e1dab3
mantolic <mantolic@digital.ai>
2021-07-05 18:27:38 +0200

    after upgrade

--- XLRelease/test github/github templ.groovy
+++ XLRelease/test github/github templ.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://MacBook-Pro.local:5517/101/#/templates/Folder708eda79151d4de6a02d16f33dd10049-Release99ef716fa077430a83d7b2ff2a0de5e6/code
 // Release version:      10.2.0-SNAPSHOT
-// Date created:         Mon Jul 05 18:21:23 CEST 2021
+// Date created:         Mon Jul 05 18:27:37 CEST 2021
 
 xlr {
   template('github templ') {
@@ -10,22 +10,7 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('task') {
-            comments {
-              comment {
-                text 'test'
-                author 'admin'
-                date Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-07-05T18:06:08+0200')
-                creationDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-07-05T18:06:08+0200')
-              }
-            }
-            attachments {
-              attachment("sql:Attachment260027719ba8477e9a9cb3f4cea4f682") {
-                filename "attachments/Attachment260027719ba8477e9a9cb3f4cea4f682-tmpl_temp.yaml"
-              }
-              attachment("sql:Attachment92b5b9d749f84e5ab612394f9922d137") {
-                filename "attachments/Attachment92b5b9d749f84e5ab612394f9922d137-🌈 Icon color.svg"
-              }
-            }
+            
           }
         }
       }
