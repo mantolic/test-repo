01e0a4b243ecaed82663b30ddefac52a0b7d8080
mantolic <mantolic@digital.ai>
2021-07-05 18:41:43 +0200

    f

---
+++ XLRelease/test github/bitbucekt1.groovy
@@ -0,0 +1,20 @@
+// Exported from:        http://MacBook-Pro.local:5517/101/#/templates/Folder708eda79151d4de6a02d16f33dd10049-Release2f83736d01e3438398c19847d1b93ecc/code
+// Release version:      10.2.0-SNAPSHOT
+// Date created:         Mon Jul 05 18:41:42 CEST 2021
+
+xlr {
+  template('bitbucekt1') {
+    folder('test github')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-07-05T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('m') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
